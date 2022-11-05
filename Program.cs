f4d769f353928061242ee752b8fef26685ab1feb
httpstephanie <110868588+httpstephanie@users.noreply.github.com>
2022-11-04 20:29:07 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp1;
+// Add input data
+var sampleData = new MLModel1.ModelInput()
+{
+    Col0 = "This restaurant was wonderful."
+};
+
+// Load model and predict output of sample data
+var result = MLModel1.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
